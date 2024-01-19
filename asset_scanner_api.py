50376ea72ee910bdd9629eab83e268cb72b6b2ac
Tigerzplace <138860538+crc-buic@users.noreply.github.com>
2024-01-19 15:09:52 +0500

    Update asset_scanner_api.py

--- asset_scanner_api.py
+++ asset_scanner_api.py
@@ -3,18 +3,17 @@ import sys
 import nmap
 import pymongo
 from flask_restful import Api, Resource
+from flask import request, Flask, jsonify
 from concurrent.futures import ThreadPoolExecutor
 from pymongo.errors import ServerSelectionTimeoutError
-from flask import request, Flask, Response, jsonify, request
 
 
 app = Flask(__name__)
 api = Api(app)
 
-class AssetScanner(Resource):
+class BaseAssetProcessor(Resource):
 
-    def __init__(self):
-
+    def __init__(self, scan_enabled=True):
         try:
             # Include credentials in the connection string
             self.client = pymongo.MongoClient("mongodb://127.0.0.1:27017/")
@@ -26,37 +25,36 @@ class AssetScanner(Resource):
 
         self.db = self.client["netspection"]
         self.collection = self.db["assets"]
+        self.scan_enabled = scan_enabled
 
-
-    def get_old(self):
-        ip_cidr = request.args.get('ip_cidr')
-        """
-        Entry point of the script.
-        :param ip_cidr: IP range to scan in CIDR notation
-        """
-        
-        if os.geteuid() != 0:
-            print('You need to be root to run this script', file=sys.stderr)
-            sys.exit(1)
-        ip_list = self.discover_assets(ip_cidr)
-
+    def process_assets(self, ip_list):
         scan_results = []
 
         with ThreadPoolExecutor() as executor:
             futures = [executor.submit(self.detect, ip) for ip in ip_list]
 
             for i, future in enumerate(futures, start=1):
-                ip = ip_list[i-1]
+                ip = ip_list[i - 1]
                 mac, detected_os = future.result()
-                print(f'{i}: IP: {ip} | Mac: {mac} | OS: {detected_os}')
-                scan_results.append({"IP": ip, "MAC": mac, "OS": detected_os})
 
-        response = {'ip list': ip_list , 'hosts detected': len(ip_list), 'scanned_results': scan_results}
-        # with open("assets.json", "w") as write_file:
-        #     json.dump(scan_results, write_file)
-        
-        return jsonify(response)
-    
+                if self.scan_enabled:
+                    try:
+                        result = self.collection.insert_one({"IP": ip, "MAC": mac, "OS": detected_os})
+                        print(f'{i}: IP: {ip} | Mac: {mac} | OS: {detected_os} | MongoDB Inserted ID: {result.inserted_id}')
+                        scan_results.append({"IP": ip, "MAC": mac, "OS": detected_os, "Status": "Success"})
+
+                    except Exception as e:
+                        print(f'{i}: IP: {ip} | Mac: {mac} | OS: {detected_os} \n MongoDB Insertion Failed. Error: {e}')
+                        scan_results.append({"IP": ip, "MAC": mac, "OS": detected_os, "Status": "Failed"})
+
+                else:
+                    print(f'{i}: IP: {ip} | Mac: {mac} | OS: {detected_os}')
+                    scan_results.append({"IP": ip, "MAC": mac, "OS": detected_os})
+
+        return scan_results
+
+
+class AssetScanner(BaseAssetProcessor):
 
     def get(self):
         ip_cidr = request.args.get('ip_cidr')
@@ -66,50 +64,18 @@ class AssetScanner(Resource):
             sys.exit(1)
 
         ip_list = self.discover_assets(ip_cidr)
-
-        scan_results = []
-
-        with ThreadPoolExecutor() as executor:
-            futures = [executor.submit(self.detect, ip) for ip in ip_list]
-
-            for i, future in enumerate(futures, start=1):
-                ip = ip_list[i-1]
-                mac, detected_os = future.result()
-
-                try:
-                    result = self.collection.insert_one({"IP": ip, "MAC": mac, "OS": detected_os})
-                    print(f'{i}: IP: {ip} | Mac: {mac} | OS: {detected_os} | MongoDB Inserted ID: {result.inserted_id}')
-                    scan_results.append({"IP": ip, "MAC": mac, "OS": detected_os, "Status": "Success"})
-
-                except Exception as e:
-                    
-                    print(f'{i}: IP: {ip} | Mac: {mac} | OS: {detected_os} \n MongoDB Insertion Failed. Error: {e}')
-                    scan_results.append({"IP": ip, "MAC": mac, "OS": detected_os, "Status": "Failed"})
-
+        scan_results = self.process_assets(ip_list)
 
         response = {'ip list': ip_list, 'hosts detected': len(ip_list), 'scanned_results': scan_results}
-
         return jsonify(response)
 
-
     def discover_assets(self, ip_cidr):
-        """
-        Discover assets on the network using Nmap.
-        :param ip_cidr: IP range in CIDR notation
-        :return: List of IP addresses
-        """
         nm = nmap.PortScanner()
         nm.scan(ip_cidr, arguments='-sn')
         ip_list = [host for host in nm.all_hosts() if nm[host].state() == 'up']
         return ip_list
 
-
     def detect(self, target):
-        """
-        Perform OS detection for a given IP address using Nmap.
-        :param target: IP address to detect the operating system
-        :return: Tuple containing the MAC address and detected operating system
-        """
         try:
             nm = nmap.PortScanner()
             machine = nm.scan(target, arguments='-v -sSU -pT:20-25,80,443-445,U:54321-54330 -O')
@@ -128,9 +94,16 @@ class AssetScanner(Resource):
             return 'Unknown', 'Unknown'
 
 
+class AssetRetrieval(BaseAssetProcessor):
+
+    def get(self):
+        stored_data = list(self.collection.find({}, {'_id': 0}))
+        response = {'stored_data': stored_data, 'total_records': len(stored_data)}
+        return jsonify(response)
+
 
 api.add_resource(AssetScanner, '/asset_scanner')
+api.add_resource(AssetRetrieval, '/retrieve_assets')
 
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run(host = "0.0.0.0", port = 9999)
+    app.run(host="0.0.0.0", port=9999)
